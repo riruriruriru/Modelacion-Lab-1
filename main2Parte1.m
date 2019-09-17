541fba43fd7f6c5a7841d4b25840bb6aa4db9540
Unknown <isaac.espinoza@usach.cl>
2019-09-16 22:49:46 -0300

    codigo listo

--- main2Parte1.m
+++ main2Parte1.m
@@ -1,6 +1,45 @@
 prompt = 'Ingrese el polinomio en su representación array:\n';
+prompt1= 'Ingrese el valor inicial x0\n';
+prompt2 = 'Ingrese el numero maximo de iteraciones\n';
+prompt3 = 'Ingrese la tolerancia\n';
 polinomio = [];
-
+x0 = 'a';
+iter = 'a';
+err = 'a';
 while length(polinomio) == 0
-    polinomio = input(prompt)
+    try 
+        polinomio = input(prompt)
+       
+    catch
+       disp("Error al ingresar polinomio, intentelo nuevamente") 
+    end
 end
+
+while isnumeric(x0) == 0|| isscalar(x0) == 0
+    try 
+        x0 = input(prompt1)
+       
+    catch
+       disp("Error al ingresar x0, intentelo nuevamente") 
+    end
+end
+
+while isnumeric(iter) == 0 || isscalar(iter) == 0 || iter <= 0 || mod(iter, 1) ~= 0
+    try 
+        iter = input(prompt2)
+       
+    catch
+       disp("Error al ingresar numero iteraciones, intentelo nuevamente") 
+    end
+end
+
+while isnumeric(err) == 0|| isscalar(err) == 0 || err < 0
+    try 
+        err = input(prompt3)
+       
+    catch
+       disp("Error al ingresar tolerancia, intentelo nuevamente") 
+    end
+end
+
+newtonRaphson(polinomio, iter, err, x0)
--- main2parte2.m
+++ main2parte2.m
@@ -3,6 +3,7 @@ vector = [];
 while length(vector) == 0 || length(vector) < 4
     try 
         vector = input(prompt)
+        sumarFunciones(vector)
     catch
        disp("Error al ingresar vector, intentelo nuevamente") 
     end
