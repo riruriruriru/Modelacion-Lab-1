346d656c023bcb019492ce791b2e672051c1b56f
Unknown <isaac.espinoza@usach.cl>
2019-09-16 23:18:30 -0300

    listo listo

--- sumarFunciones.m
+++ sumarFunciones.m
@@ -1,16 +1,20 @@
+%Función Sumar Funciones.
+%ENTRADA: * Vector
+%SALIDA: Raiz cuadrada de la suma de los 4 elementos mayores menos la raiz cuadrada de la suma de los 4 elementos menores    
 function [resultado] = sumarFunciones(vector)
 if length(vector) < 4
     disp("El vector ingresado tiene menos de 4 elementos")
     return
 end
+%Se ordena el vector de forma ascendiente y descendiente
 mayores = sort(vector, 'descend');
 mayores = mayores(1:4)
 menores = sort(vector, 'ascend');
 menores = menores(1:4)
+%Se separan los 4 elementos mayores y menores
+%Se suman los 4 elementos mayores y los 4 menores
 sumaMayores = sum(mayores);
 sumaMenores = sum(menores);
-if sumaMayores < 0 || sumaMenores < 0
-    disp("Valores ingresados no son validos")
-    return
-end
+%Se calcula el resultado correspondiente a la resta de las raices cuadradas
+%de la suma de los mayores menos la suma de los menores
 resultado = sqrt(sumaMayores) - sqrt(sumaMenores);
